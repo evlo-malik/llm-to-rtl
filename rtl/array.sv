7ece5a68436ff1cd7f3507a762659758dfa07a52
Malik Evloev <evlo.malik@gmail.com>
2026-09-07 11:22:35 +0400

    added wires to the array

--- rtl/array.sv
+++ rtl/array.sv
@@ -5,3 +5,6 @@ module array #(parameter int unsigned N = 3) (
     input logic signed [7:0] w_in [N],
     output logic signed [31:0] result [N]
 );
+wire signed [7:0] x_wire [N][N];
+wire signed [7:0] w_wire [N][N];
+wire signed [31:0] result_wire [N][N];
